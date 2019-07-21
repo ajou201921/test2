c0541e473dc4ae34ed116fc310e54c4e0f0d315a
sujin <tnwls9712@ajou.ac.kr>
2019-07-21 18:16:11 +0900

    3개 TEST 추가

--- src/test/java/BookTest.java
+++ src/test/java/BookTest.java
@@ -1,6 +1,9 @@
 import org.hamcrest.core.Is;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertFalse;
@@ -42,4 +45,43 @@ public class BookTest{
         when(book.getNum()).thenReturn(1);
         assertThat(book.getNum(), Is.is(1));
     }
+
+    @Test //황수진 method 1
+    public void 북리스트사이즈테스트(){
+        List<Book> books =new ArrayList<Book>();
+        books.add(new Book(1234,"HarryPotter 1","J.K"));
+        books.add(new Book(1235,"HarryPotter 2","J.K"));
+        books.add(new Book(1236,"HarryPotter 3 ","J.K"));
+        assertThat(books.size(),is(3));
+    }
+
+    @Test //황수진 method2
+    public void Timeout테스트(){
+
+        Book book = mock(Book.class);
+        book.setTitle("Harry Potter");
+        verify(book, timeout(100).atLeast(1)).setTitle(anyString());
+    }
+
+    @Test //황수진 method3
+    public void 도서관리숫자(){
+
+        List<Book> books =new ArrayList<Book>();
+        List<Book> books_1200 =new ArrayList<Book>();
+
+        books.add(new Book(1000,"HarryPotter Story","J.K"));
+        books.add(new Book(1235,"HarryPotter 1","J.K"));
+        books.add(new Book(1236,"HarryPotter 2 ","J.K"));
+
+        for(int i=0;i<books.size();i++){
+            if(books.get(i).getNum()>=1200){
+                books_1200.add(books.get(i));
+            }
+        }
+        assertTrue(books_1200.get(0).getNum()>=1200);
+        assertTrue(books_1200.get(1).getNum()>=1200);
+
+    }
+
+
 }
